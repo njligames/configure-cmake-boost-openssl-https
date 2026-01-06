ce6e91ecaf21925aede2783543ac0eae63fc9192
James Folk <jamesfolk1@gmail.com>
2026-01-06 09:13:17 -0500

    first pass and works

--- environment/main.cpp
+++ environment/main.cpp
@@ -1,5 +1,7 @@
 #include <boost/asio.hpp>
 #include <boost/asio/ssl.hpp>
+#include <boost/asio/ssl/host_name_verification.hpp>
+#include <openssl/ssl.h>
 #include <iostream>
 #include <string>
 
@@ -7,11 +9,9 @@ using boost::asio::ip::tcp;
 
 int main() {
     try {
-        // IO context and SSL context
         boost::asio::io_context io;
         boost::asio::ssl::context ctx(boost::asio::ssl::context::tls_client);
 
-        // Configure TLS options
         ctx.set_default_verify_paths();
         ctx.set_options(
             boost::asio::ssl::context::default_workarounds |
@@ -21,20 +21,25 @@ int main() {
             boost::asio::ssl::context::no_tlsv1_1
         );
 
-        // Create SSL socket
         boost::asio::ssl::stream<tcp::socket> socket(io, ctx);
-        socket.set_verify_mode(boost::asio::ssl::verify_peer);
-        socket.set_verify_callback(boost::asio::ssl::rfc2818_verification("example.com"));
 
-        // Resolve host and connect
+        // REQUIRED: certificate + hostname verification
+        socket.set_verify_mode(boost::asio::ssl::verify_peer);
+        socket.set_verify_callback(
+            boost::asio::ssl::host_name_verification("example.com")
+        );
+
+        // REQUIRED: set SNI (this is what was missing)
+        if (!SSL_set_tlsext_host_name(socket.native_handle(), "example.com")) {
+            throw std::runtime_error("Failed to set SNI hostname");
+        }
+
         tcp::resolver resolver(io);
-        auto endpoints = resolver.resolve("example.com", "https");
+        auto endpoints = resolver.resolve("example.com", "443");
         boost::asio::connect(socket.lowest_layer(), endpoints);
 
-        // Perform SSL handshake
         socket.handshake(boost::asio::ssl::stream_base::client);
 
-        // Send HTTP GET request
         std::string request =
             "GET / HTTP/1.1\r\n"
             "Host: example.com\r\n"
@@ -42,17 +47,14 @@ int main() {
 
         boost::asio::write(socket, boost::asio::buffer(request));
 
-        // Read response status line
         boost::asio::streambuf response;
         boost::asio::read_until(socket, response, "\r\n");
 
         std::istream response_stream(&response);
         std::string http_version;
         unsigned int status_code;
-        std::string status_message;
 
         response_stream >> http_version >> status_code;
-        std::getline(response_stream, status_message);
 
         std::cout << "HTTP status code: " << status_code << std::endl;
     }
